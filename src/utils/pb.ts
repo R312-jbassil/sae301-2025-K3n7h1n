eff8a57e4e490c0521b2189190e6d455293bdc9e
Enzo Locatelli <enzort70290@gmail.com>
2025-11-05 13:29:45 +0100

    fix pocketbase

--- src/utils/pb.ts
+++ src/utils/pb.ts
@@ -1,5 +1,5 @@
 import PocketBase from 'pocketbase';
-import type { TypedPocketBase } from "./pocketbase-types";
+import type { TypedPocketBase } from "./pocketbase-types.ts";
 var path='';
 if(import.meta.env.MODE === 'development')
     path = 'http://localhost:8090'    //localhost = machine de dev
